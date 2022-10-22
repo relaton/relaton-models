dfad3f10e5472ac9853193ab6eda6f2ab81e161b
Nick Nicholas <opoudjis@gmail.com>
2022-10-22 15:14:34 +1100

    Separate grammar for extension point: https://github.com/relaton/relaton-models/issues/43

--- grammars/make.sh
+++ grammars/make.sh
@@ -5,3 +5,5 @@ if [[ ! -d jing-trang ]]; then
   cd ..
 fi
 java -jar jing-trang/build/trang.jar -I rnc -O rng biblio.rnc biblio.rng
+java -jar jing-trang/build/trang.jar -I rnc -O rng biblio-standoc.rnc biblio-standoc.rng
+java -jar jing-trang/build/trang.jar -I rnc -O rng biblio-compile.rnc biblio-compile.rng
