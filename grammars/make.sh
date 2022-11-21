c04072e065350798684994123a96f8ad65a466b2
Nick Nicholas <opoudjis@gmail.com>
2022-11-21 23:11:17 +1100

    include basicdoc in biblio-compile.rnc: https://github.com/metanorma/metanorma-model-iso/issues/73

--- grammars/make.sh
+++ grammars/make.sh
@@ -1,3 +1,13 @@
+echo "Updating submodules..."
+
+rm -f basicdoc-models/grammars/basicdoc.rng
+git submodule update
+
+cd basicdoc-models/grammars
+git checkout main && git pull
+cd ../..
+cp basicdoc-models/grammars/basicdoc.rnc .
+
 if [[ ! -d jing-trang ]]; then
   git clone https://github.com/relaxng/jing-trang.git
   cd jing-trang
